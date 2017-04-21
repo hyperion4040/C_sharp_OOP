bc550453a28964bf7cb994e82ae7d1626f67e122
hyperion4040 <hyperion1987@wp.pl>
2017-04-21 22:39:27 +0200

    new projects to present delegates

---
+++ BookStore/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public delegate void bookDelegate(string message);
+
+public delegate String bookDelegateSecond();
+
+namespace BookStore
+{
+
+
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int numberOfBooks;
+        private int numberOfPages;
+
+        public int NumberOfPages
+        {
+            get { return numberOfPages; }
+            set { numberOfPages = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public int NumberOfBooks
+        {
+            get { return numberOfBooks; }
+            set
+            {
+                if(value > -1)
+                numberOfBooks = value;
+                else
+                {
+                    throw new Exception("Number of book can't be smaller than 0");
+                }
+            }
+        }
+
+        public Book(string title, string author, int numberOfBooks,int numberOfPages)
+        {
+            this.title = title;
+            this.author = author;
+            this.numberOfBooks = numberOfBooks;
+            this.numberOfPages = numberOfPages;
+        }
+
+        public void countSumOfPagesOfAllBooks(string name)
+        {
+            Console.WriteLine("{0}, your book has {1} pages",name, this.numberOfPages );
+
+        }
+
+        public string nameOfBook()
+        {
+            return "This book title is " + title;
+        }
+    }
+}
---
+++ BookStoreMain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BookStore;
+
+namespace BookStoreMain
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Book book = new Book("Tolkien","Hobbit",12,1250);
+            bookDelegate bookDelegate = new bookDelegate(book.countSumOfPagesOfAllBooks);
+            bookDelegate("Adrian");
+            bookDelegateSecond bookDelegateSecond = new bookDelegateSecond(book.nameOfBook);
+            Console.WriteLine(bookDelegateSecond.Invoke());
+        }
+    }
+}
